528fff35e75c9524db6288bebaf8a59550649093
Akihi-g <akihirokun@gmail.com>
2020-12-29 15:40:06 +0900

    Second commit

--- Lecture1.R
+++ Lecture1.R
@@ -2,9 +2,15 @@
 #Wide format
 #Integer, binary, categorical variables
 #Multilevel
-
+#Set up libraries
 library(tidyverse)
 library(mice)
+url <- "https://cran.r-project.org/src/contrib/Archive/norm2/norm2_2.0.3.tar.gz"
+pkgFile <- "norm2_2.0.3.tar.gz"
+download.file(url = url, destfile = pkgFile)
+install.packages(pkgs=pkgFile, type="source", repos=NULL)
+library(norm2)
+#Read the sample file
 dat <- read_csv("C:/Users/akihi/Downloads/Sample_data.csv",
                 locale = locale(encoding = "SHIFT-JIS"),
                 col_types = cols(
@@ -27,11 +33,11 @@ dat <- read_csv("C:/Users/akihi/Downloads/Sample_data.csv",
                 na = "NA") #Look at the errors
 dat %>% glimpse()
 md.pattern(dat)
-###################################################################################################################################
-#1. Generate m complete data sets.(mice -> mids {a multiply imputed data set})
-#2. Analyze separately using any standard complete-data technique. (with -> mira {a multiple imputed repeated analysis})
-#3. Integrate into an overall set of results using combination rules. (pool -> mipo {a multiply imputed pooled outcome} )
-###################################################################################################################################
+############################################################################################################################
+#1. Generate m complete data sets.(mice -> mids {a multiply imputed data set})                                            ##
+#2. Analyze separately using any standard complete-data technique. (with -> mira {a multiple imputed repeated analysis})  ##
+#3. Integrate into an overall set of results using combination rules. (pool -> mipo {a multiply imputed pooled outcome} ) ##
+############################################################################################################################
 ###First step: creating mids object###
 #mice: default "pmm", "logreg (Bayesian logistic regression)", "polyreg (Bayesian polytomous regression)"
 #maxit: iterations of the Gibbs sampler
@@ -47,34 +53,41 @@ dat_i <- mice(dat, m = 100, maxit = 20, print = FALSE, seed = 1234)
 plot(dat_i)
 #Standard imputation vs multilevel imputation
 #Only support two-level
-###Second step###
+###Second and third step###
 com <- complete(dat_i, 1)
-fit1 <- lm(wbc ~ age, com)
-s <- summary(fit)
-s$coefficients
+fit1 <- glm(death ~ age, com, family = binomial(link = "logit"))
+s <- summary(fit1)
+exp(s$coefficients)
 #Manual approach
 est <- se <- vector(length = dat_i$m, mode = "list")
 for (i in 1:dat_i$m) {
   com <- complete(dat_i, i)
-  fit2 <- lm(wbc ~ age, com)
+  fit2 <- glm(death ~ age, com, family = binomial(link = "logit"))
   s <- summary(fit2)
   est[[i]] <- s$coefficients[2, 1]
   se[[i]] <- s$coefficients[2, 2]
   }
-library(mitools)
-miinf <- MIcombine(est, se)
+miinf <- miInference(est, se)
 print(miinf)
-miinf$coefficients + c(-1, 1) * qt(0.975, miinf$df) * miinf$variance
+exp(miinf$est)
+exp(miinf$std.err)
 #Using with & pool
 #Dy default, correct for small samples
-mira <- with(dat_i, lm(wbc ~ age))
+mira <- with(dat_i, glm(death ~ age,  family = binomial))
 result <- summary(pool(mira))
 print(result[, 1:5])
-
-
-
-
-
-
-
+##Model diagnostics
+imp.wbc <- data.frame(dat_i$imp$wbc)
+imp.wbc.long <- reshape(imp.wbc,
+                        varying = list(c(paste0("X", 1:100))),
+                        direction = "long",
+                        v.names = "wbc",
+                        times = 1:100,
+                        timevar = "IMP",
+                        idvar = "id")
+sub <- subset(imp.wbc.long, imp.wbc.long$IMP <= 20)
+boxplot(wbc  ~  IMP, sub, xlab = "Imputation number", ylab = "WBC", pch = 20, col = "gray80")
+hist(sub$wbc, col = gray(0.1, alpha = 0.5), freq = FALSE, xlab = "Distribution of WBC", main = "")
+hist(imp.wbc.long$wbc, col = gray(0.8, alpha = 0.5), freq = FALSE, add = TRUE)
+legend("topright", legend = c("observed", "imputed"), fill = c(gray(0.1, alpha = 0.5), gray(0.8, alpha = 0.5)))
 
